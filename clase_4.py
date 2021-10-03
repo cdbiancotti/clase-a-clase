710eff7fc7e246d410898c502bd349a4e181c5ed
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-10-03 20:33:59 -0300

    Added clase 4

---
+++ clase_4.py
@@ -0,0 +1,60 @@
+# numero1 = 2
+# numero2 = 4
+# # TODO: agregar otro numero
+# numero3 = 15
+ 
+# if numero1 == 13:
+#     # TODO: Agregar las lineas correspondientes a esta condicional
+#     pass
+# print('Hola')
+
+# if numero1 < numero2:
+#     print(numero1 + numero2)
+# elif numero2 < numero3:
+#     print(numero2 * numero1)
+# elif numero1 < numero3:
+#     print('numero1 es menor a numero3')
+# else:
+#     print('Son iguales')
+
+# if numero1 > numero2:
+#     print(numero1 + numero2)
+# elif numero1 < numero2:
+#     print(numero2 * numero1)
+# elif numero1 < numero3:
+#     print('numero1 es menor a numero3')
+# else:
+#     print('Son iguales')
+
+# if numero1 < numero2:
+#     print(numero2 * numero1)
+# if numero1 == numero2:
+#     print('Son iguales')
+
+# edad = int(input("¿Cuál es tu edad? "))
+# if edad < 16:
+#    print ("Eres menor de edad.")
+# else:
+#    print("Eres mayor de edad.")
+
+# print('P' > 'p')
+
+# edad=int(input("ingrese su edad:"))
+# if edad > 16:
+#    print("si es mayor de edad")
+# else:
+#    print("es menor de edad")
+
+# print('PRUEBA'.lower())
+
+# nombre = input("cómo te llamás? ").lower()
+# preferencia = input("marvel o capcom? (M o C correspondiente)")
+# preferencia = preferencia.upper()
+# if (preferencia == "M" and nombre < "m") or (preferencia == "C" and nombre > "n"):
+#     grupo = "A"
+# else:
+#     grupo = "B"
+# print("tu grupo es " + grupo)
+
+print(input("cómo te llamás? ").title())
+
