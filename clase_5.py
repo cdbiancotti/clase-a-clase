f796108874ffcf222c3a790d2f096d0ac1380af4
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-10-03 20:30:36 -0300

    Added clase 5

---
+++ clase_5.py
@@ -0,0 +1,107 @@
+# if 1 > 2:
+#     print('Se ejecuto')
+
+# print('No se ejecuto')
+# a = 0
+# while 1 < 2:
+#     print('Se ejecuto')
+#     a += 1
+#     print(a)
+# a = True
+# while a:
+#     print('Se ejecuto')
+#     break
+
+# if 1 < 2:
+#     pass
+
+# while 1 < 2:
+#     break
+
+# a = True
+# b = 0
+# while a:
+#     print(b)
+#     if b > 15:
+#         a = False
+#     if b == 14:
+#         break
+#     b +=1
+# else:
+#     print('No se ejecuto')
+
+# a = True
+# b = 0
+# while a:
+#     print(a)
+#     print(b)
+#     if b == 2:
+#         pass
+#     if b == 4:
+#         break
+#     b += 1
+#     if b == 3:
+#         continue
+#     print('Por aca no se paso')
+# a = [1,2,3,4,5,6]
+# b = 0
+# while a[b]:
+#     b += 1
+
+# a = [1,2,3,4,5,6]
+
+# for numero in a:
+#     if numero == 6:
+#         break
+#     if numero % 2:
+#         continue
+#     print(numero)
+
+# texto = 'Este es un texto'
+# print(texto[3::2])
+
+# print(list(range(3,13,4)))
+
+# numero = int(input('Ingresa hasta que numero queres sumar? '))
+# suma = 0
+# for num in range(numero):
+#     num += 1
+#     suma += num
+
+# print(suma)
+
+# suma = 0
+# while True:
+#     numero = int(input('Ingresa un numero a sumar? ( si quiere terminar ingrese 0 )'))
+#     if numero != 0:
+#         suma += numero
+#     else:
+#         break
+# print(suma)
+
+# for caracter in 'abcdefg':
+#     print(caracter)
+# range(5)
+# [0,1,2,3,4]
+
+# for indice, palabra in enumerate([32, 'correr', True, 'tabla', 'cuidado']):
+#     print(str(indice), str(palabra))
+#     break
+# else:
+#     print('Paso por el else')
+
+# paises = ['Canada', 'USA', 'Mexico', 'Australia', 'Argentina', 'China', 'India']
+
+# for indice, pais in enumerate(paises):
+#     print(indice+1, pais)
+# suma = 0
+# for numero in range(1, 101):
+#     if numero % 2 == 0:
+#         suma += numero
+
+# print(suma)
+
+# print(list(range(10, 0, -1)))
+
+# numero = input('Ingresa un numero: ')
+# print(len(numero))
