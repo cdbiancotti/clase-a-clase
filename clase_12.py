25aefe13c3dc501c0e59066c5c15ad8cbbaea391
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-10-20 22:35:33 -0300

    Added clase 12 ( parte de consulta y observaciones de objetos )

---
+++ clase_12.py
@@ -0,0 +1,46 @@
+# Teoria... boringgggggggg
+
+var = 'Hola'
+
+# print(var)
+
+dic = {
+    'key': 'value'
+}
+
+# dic.key
+
+# print(dic['key'])
+
+# var_upper = var.upper()
+print(type('Hola'))
+var_upper = 'Hola'.upper()
+
+# marca = 'Ford'
+# color = 'rojo'
+
+# def funcion():
+#     return
+# dato >>pasa a ser un>> objeto >>se genera con una>> clase
+
+# clase(general)
+# objeto(especifico)
+# instancia
+
+
+# print(var_upper)
+
+# def test(var1, var2):
+#     ...
+
+# test(2, 1)
+
+
+# def test(var1):
+#     n = 1
+#     if var1 != 0:
+#         return n + test(var1 - 1)
+#     else:
+#         return n
+
+# print(test(5))
